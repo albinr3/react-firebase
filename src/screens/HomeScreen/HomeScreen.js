d3a0dc80f7cb3349fc25ed6ed447c829a72054be
Albin Rodriguez <albinmrodriguez@gmail.com>
2024-01-19 00:14:57 -0400

    s

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Text, View } from 'react-native'
 
 export default function HomeScreen(props) {
+  console.log("info nueva ", props.params)
   return (
     <View>
       <Text>ggg</Text>
--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { Image, Pressable, Text, TextInput, View } from 'react-native'
+import { Image, Pressable, Text, TextInput, View, ActivityIndicator} from 'react-native'
 //import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { FIREBASE_AUTH, FIREBASE_DB } from '../../../firebaseConfig';
-import { collection, addDoc, doc, getDocs} from "firebase/firestore"; 
+import { collection, setDoc, getDocs, doc} from "firebase/firestore"; 
 
 
 
@@ -13,49 +13,71 @@ export default function RegistrationScreen({ navigation }) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  
+  const [isLoading, setIsLoading] = useState(false);
+
+  /**
+   * Navigates to the Login screen.
+   */
   const onFooterLinkPress = () => {
     navigation.navigate('Login')
   }
   
+  /**
+   * Handles user registration.
+   */
   const onRegisterPress = () => {
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      alert("Passwords don't match.");
+      return;
+    }
+
+    // Activate loading animation
+    setIsLoading(true);
+
+    // Create user with email and password using Firebase authentication
     createUserWithEmailAndPassword(FIREBASE_AUTH, email, password)
       .then(async (userCredential) => {
-        // Signed up 
+        // User signed up successfully 
         const uid = userCredential.user.uid;
         const dataUser = {
           id: uid,
           email,
           fullName
         };
-        const addUser = await addDoc(collection(FIREBASE_DB, "users"), {
+
+        // Add user data to Firestore database
+        const usersRef = collection(FIREBASE_DB, "users");
+        const addUser = await setDoc(doc(usersRef, uid), {
           dataUser
         }); 
+        // Navigate to Home screen with user data
         navigation.navigate('Home', {user: dataUser})      
       })
       .catch((error) => {
-        const errorCode = error.code;
         console.error("Error al crear usuario", error);
         // ..
+      })
+      .finally(() => {
+        // Desactivar la animación de carga, ya sea que haya tenido éxito o no
+        setIsLoading(false);
       });
     
   }
 
-  const onNav = () => {
-    navigation.navigate('Home');
-  }
-  //consultar base de datos
- // const onConsultPress = async () => {
-    // try {
-    //   const querySnapshot = await getDocs(collection(FIREBASE_DB, "users"));
   
-    //   querySnapshot.forEach((doc) => {
-    //     console.log(`${doc.id} => ${JSON.stringify(doc.data(), null, 2)}`);
-    //   });
-    // } catch (error) {
-    //   console.error("Error al consultar la base de datos:", error);
-    // }
-  //}
+  // Uncomment the following code to enable database query
+ const onConsultPress = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(FIREBASE_DB, "users"));
+  
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data(), null, 2)}`);
+      });
+    } catch (error) {
+      console.error("Error al consultar la base de datos:", error);
+    }
+  }
   
   return (
     <View style={styles.container}>
@@ -64,6 +86,7 @@ export default function RegistrationScreen({ navigation }) {
           style={styles.logo}
           source={require('../../../assets/icon.png')}
         />
+        {/* Input fields for user registration */}
         <TextInput
           style={styles.input}
           placeholder="Fullname"
@@ -102,16 +125,22 @@ export default function RegistrationScreen({ navigation }) {
           underlineColorAndroid="transparent"
           autoCapitalize="none"
         />
+        {/* Display loading indicator or registration button based on isLoading state */}
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <Pressable
+            style={styles.button}
+            onPress={() => onRegisterPress()}>
+            <Text style={styles.buttonTitle}>Create account</Text>
+          </Pressable>
+      )}
         <Pressable
           style={styles.button}
-          onPress={() => onRegisterPress()}>
-          <Text style={styles.buttonTitle}>Create account</Text>
-        </Pressable>
-        <Pressable
-          style={styles.button}
-          onPress={() => onNav()}>
+          onPress={() => onConsultPress()}>
           <Text style={styles.buttonTitle}>navegar</Text>
         </Pressable>
+        {/* Footer text with link to Login screen */}
         <View style={styles.footerView}>
           <Text style={styles.footerText}>Already got an account? <Text onPress={onFooterLinkPress} style={styles.footerLink}>Log in</Text></Text>
         </View>
